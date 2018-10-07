80cde8553b301c2eae6af9f568d5070b4c710be1
Christian Passold <passold.christian@hotmail.com>
2018-10-07 17:17:41 -0300

    Ajustes para exibição do erro e do sucesso da compilação

--- src/main/java/core/AnalisadorLexico.java
+++ src/main/java/core/AnalisadorLexico.java
@@ -1,19 +1,15 @@
 package core;
 
-import gals.LexicalError;
-import gals.Lexico;
-import gals.ScannerConstants;
-import gals.Token;
+import gals.*;
 
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class AnalisadorLexico {
 
     private final JTextArea jMensagens;
-    private final char[] specialChars = new char[]{'\n', '\t', ' '};
-
 
     public AnalisadorLexico(JTextArea jMensagens) {
         this.jMensagens = jMensagens;
@@ -33,28 +29,106 @@ public class AnalisadorLexico {
             palavrasReservadasInvalidas.add(palavrasReservada.toLowerCase());
         }
 
-        Token token = null;
+        Token token;
         try {
+            MensagemSucesso mensagemSucesso = new MensagemSucesso();
 
             while ((token = lexico.nextToken()) != null) {
                 if (palavrasReservadasInvalidas.contains(token.getLexeme()) && !(token.getId() >= 7 && token.getId() <= 21)) {
                     throw new LexicalError("palavra reservada inválida", token.getPosition());
                 }
 
-
-                System.out.println(token.getLexeme());
+                mensagemSucesso.add(getLinha(codigo, token.getPosition()), getClasseById(token.getId()), token.getLexeme());
             }
 
-            jMensagens.setText("Compilado com sucesso");
+            jMensagens.append(mensagemSucesso.getMensagem());
+
+
         } catch (LexicalError e) {
             //todo: Pegar todo o nome do identificador com problema
-            final String[] split = codigo.split("[^\\w+$]+", e.getPosition());
-            jMensagens.setText(String.format("Erro na linha %d - %s %s",
+
+
+            System.out.println(e.getPosition());
+            System.out.println(e.getMessage());
+
+            jMensagens.setText(String.format("Erro na linha %d - %s",
                     getLinha(codigo, e.getPosition()),
-                    split[split.length - 1].substring(e.getPosition()),
                     e.getMessage()
             ));
         }
     }
 
+    private static class MensagemSucesso {
+
+        private List<Integer> linha = new LinkedList<>();
+        private List<String> classe = new LinkedList<>();
+        private List<String> lexema = new LinkedList<>();
+
+        private int position = 0;
+
+        void add(int linha, String classe, String lexema) {
+            this.linha.add(position, linha);
+            this.classe.add(position, classe);
+            this.lexema.add(position, lexema);
+
+            position++;
+        }
+
+        String getMensagem() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder
+                    .append("Linha")
+                    .append('\t')
+                    .append("Classe")
+                    .append('\t')
+                    .append('\t')
+                    .append('\t')
+                    .append("Lexema")
+                    .append('\n');
+
+            for (int i = 0; i < position; i++) {
+                stringBuilder
+                        .append(this.linha.get(i))
+                        .append('\t')
+                        .append(this.classe.get(i))
+                        .append('\t')
+                        .append('\t')
+                        .append('\t')
+                        .append(this.lexema.get(i))
+                        .append('\n');
+            }
+
+            return stringBuilder.toString();
+        }
+
+    }
+
+    private String getClasseById(int id) {
+
+        if (id == Constants.t_id) {
+            return "identificador";
+        }
+
+        if (id == Constants.t_conliteral) {
+            return "constante literal";
+        }
+
+        if (id == Constants.t_concar) {
+            return "constante caractere";
+        }
+        if (id == Constants.t_connum) {
+            return "constante numérica";
+        }
+
+        if (id >= 7 && id <= 21) {
+            return "palavra reservada";
+        }
+
+        if (id >= 22 && id <= 40) {
+            return "símbolo especial";
+        }
+
+        return null;
+    }
 }
